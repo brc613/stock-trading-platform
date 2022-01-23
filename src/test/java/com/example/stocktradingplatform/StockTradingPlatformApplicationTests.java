d37c7f405579d449cbc8f8dd06e5a200ef9f075d
brc613 <brc613@gmail.com>
2022-01-22 23:13:33 -0500

    initial commit

---
+++ src/test/java/com/example/stocktradingplatform/StockTradingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stocktradingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTradingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
